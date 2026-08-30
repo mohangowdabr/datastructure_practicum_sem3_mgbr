c1a457396cee9224e90d183add0b8edbaac2ced5
MOHAN GOWDA B R <mohangowda0614@gmail.com>
2026-08-30 18:26:25 +0530

    Update 3a.cpp

--- 3a.cpp
+++ 3a.cpp
@@ -1,38 +1,34 @@
-    #include <iostream>
-    using namespace std;
+#include <iostream>
+using namespace std;
 
-    void towerOfHanoi(int n, char source, char auxiliary, char destination)
-    {
-        if (n == 1)
-        {
-            cout << "Move disk 1 from " << source
-                << " to " << destination << endl;
-            return;
-        }
-
-        towerOfHanoi(n - 1, source, destination, auxiliary);
-
-        cout << "Move disk " << n << " from " << source
-            << " to " << destination << endl;
-
-        towerOfHanoi(n - 1, auxiliary, source, destination);
+void hanoi(int n, char from, char aux, char to) {
+    if (n == 1) {
+        cout << "Move disk 1 from " << from << " to " << to << "\n";
+        return;
     }
+    
+    // Move n-1 disks from 'from' to 'aux' using 'to'
+    hanoi(n - 1, from, to, aux);
+    
+    // Move largest disk from 'from' to 'to'
+    cout << "Move disk " << n << " from " << from << " to " << to << "\n";
+    
+    // Move n-1 disks from 'aux' to 'to' using 'from'
+    hanoi(n - 1, aux, from, to);
+}
 
-    int main()
-    {
-        int n;
-
-        cout << "Enter number of disks: ";
-        cin >> n;
-
-        if (n <= 0)
-        {
-            cout << "Number of disks must be greater than 0.";
-            return 0;
-        }
-
-        cout << "\nSteps to solve Tower of Hanoi:\n";
-        towerOfHanoi(n, 'A', 'B', 'C');
-
+int main() {
+    int n;
+    cout << "Disks: ";
+    cin >> n;
+    
+    if (n <= 0) {
+        cout << "Invalid!\n";
         return 0;
     }
+    
+    cout << "Steps:\n";
+    hanoi(n, 'A', 'B', 'C');
+    
+    return 0;
+}
