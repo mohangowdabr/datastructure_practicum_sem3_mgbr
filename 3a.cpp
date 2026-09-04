b635246342b316323cdb3619b22e4d97ded7b6a8
MOHAN GOWDA B R <mohangowda0614@gmail.com>
2026-09-04 12:21:31 +0530

    Update 3a.cpp

--- 3a.cpp
+++ 3a.cpp
@@ -1,1 +1,27 @@
+#include <iostream>
+using namespace std;
+void towerOfHanoi(int n, char source, char destination, char auxiliary)
+{
+	if (n == 0)
+		return;
+	towerOfHanoi(n - 1, source, auxiliary, destination);
+	cout << "Move disk " << n << " from " << source << " to " << destination << endl;
+	towerOfHanoi(n - 1, auxiliary, destination, source);
+}
+int main(){
+
+	int n;
+	cout << "Enter disks: ";
+	cin >> n;
+	if (n <= 0)
+	{
+		cout << "Must be > 0" << endl;
+		return 0;
+	}
+	cout << "\nSolution:\n"; 
+	towerOfHanoi(n, 'A', 'C', 'B');
+	cout << "\nTotal moves: " << (1LL << n) - 1 << endl;
+	return 0;
+}
+
 
